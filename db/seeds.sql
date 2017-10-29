fd411931b6e4e8bed8e558f4250b987b5eb226a1
Jose Arancibia <josearancibia89@gmail.com>
2017-10-29 15:06:45 -0400

    uploading Eat That Burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Texas Burger');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers(burger_name) VALUES ('Cheddar Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Plain Old Hamburger');
