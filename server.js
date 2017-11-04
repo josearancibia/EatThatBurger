b0285fbe4a60ad90ed57c2a08545d117c327caba
Jose Arancibia <josearancibia89@gmail.com>
2017-11-04 16:58:38 -0400

    adding server.js file

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+// Set Handlebars.
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+var port = process.env.PORT || 8889;
+app.listen(port);
+console.log("connected to " + port);
